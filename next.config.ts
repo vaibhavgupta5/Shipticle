765053cf00f413e7bac7847cd5d87abb4fa03757
vaibhavgupta5 <vaibhavgupta.v890@gmail.com>
2026-06-23 15:30:27 +0530

    fix firebase auth

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  serverExternalPackages: ["firebase-admin"],
+  transpilePackages: ["firebase-admin", "jwks-rsa"],
 };
 
 export default nextConfig;
